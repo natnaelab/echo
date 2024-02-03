c3405e0110626e79c95e2a30cd062e7e4149f5d7
Natnael Abrham <natnael08@protonmail.com>
2024-02-03 15:17:25 +0300

    Update settings, render config

--- core/settings.py
+++ core/settings.py
@@ -17,8 +17,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env("DEBUG")
-# ALLOWED_HOSTS = env("ALLOWED_HOSTS")
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env("ALLOWED_HOSTS")
 CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=[])
 
 # Application definition
